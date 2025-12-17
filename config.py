d314bb50a01262dfcd5d8cffb7a1792c02e4f761
{remo} <moonwalker1092@gmail.com>
2025-12-17 15:53:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 # Recommended
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8120086484:AAEsjovfDcT8R9RNHj4gJedKgRaLRHrGaJ4")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", "27693340"))
 API_HASH = os.environ.get("API_HASH", "1056193e68c138ee16edc02578c559e1")
 
